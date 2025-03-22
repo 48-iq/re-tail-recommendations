a388ad20e5459ac38e3fdbcd7e74709083a9a8d5
48-iq <ivn25ov@mail.ru>
2025-03-22 14:34:50 +0400

    init

---
+++ src/test/java/dev/ilya_anna/recomendation_service/RecommendationServiceApplicationTests.java
@@ -0,0 +1,10 @@
+package dev.ilya_anna.recomendation_service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationServiceApplicationTests {
+
+
+
+}
